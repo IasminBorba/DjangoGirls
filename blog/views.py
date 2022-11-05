98dcbad0dd0dde2ab836b5e6b6373cb18a6657ef
Iasmin Borba <icb2@ic.ufal.br>
2022-11-05 17:32:06 -0300

    Add files via upload

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import *
+from django.utils import timezone 
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html',{'posts': posts})
+    
